a460a6efb0398a6b11ecc4e018e2a60424c4acf9
Jo Wagner <jowagner@limago.de>
2025-12-15 14:43:27 +0100

    initial commit

---
+++ Tag1_02SchweinProjekt/tests/services/MultImpl.h
@@ -0,0 +1,27 @@
+//
+// Created by JoachimWagner on 19.08.2024.
+//
+
+#pragma once
+
+
+#include "Mult.h"
+
+
+class MultImpl: public Mult {
+public:
+    long mult(int a, int b) const override {
+        long result = 0;
+
+        if( a > b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        for(int i = 0 ; i < a; i++) {
+            result += b;
+        }
+        return result;
+    }
+};
